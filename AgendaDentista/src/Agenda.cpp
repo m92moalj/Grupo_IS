0fdbcbd733e612f9e4d1c87533004f46f6ed1fe9
Eduardo Roldan Pijuan <i32ropie@uco.es>
2014-12-03 19:28:49 +0100

    Clase Agenda.

---
+++ AgendaDentista/src/Agenda.cpp
@@ -0,0 +1,21 @@
+/*
+ * Agenda.cpp
+ *
+ *  Created on: 03/12/2014
+ *      Author: i32ropie
+ */
+
+#include "Agenda.h"
+
+namespace dentista {
+
+Agenda::Agenda() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Agenda::~Agenda() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace dentista */
---
+++ AgendaDentista/src/Agenda.h
@@ -0,0 +1,20 @@
+/*
+ * Agenda.h
+ *
+ *  Created on: 03/12/2014
+ *      Author: i32ropie
+ */
+
+#ifndef AGENDA_H_
+#define AGENDA_H_
+
+namespace dentista {
+
+class Agenda {
+public:
+	Agenda();
+	virtual ~Agenda();
+};
+
+} /* namespace dentista */
+#endif /* AGENDA_H_ */
