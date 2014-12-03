6e87e94ccf3aed1b02c07052ebe1fa6720b3bc26
Eduardo Roldan Pijuan <i32ropie@uco.es>
2014-12-03 18:48:14 +0100

    Proyecto recién empezado.

---
+++ AgendaDentista/src/AgendaDentista.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : AgendaDentista.cpp
+// Author      : OranGITops
+// Version     :
+// Copyright   : OranGITops
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "OranGITops" << endl; // prints OranGITops
+	return 0;
+}
