21437788afcf8b1df4820ded2fba9d097a445a0c
Eduardo Roldan Pijuan <i32ropie@uco.es>
2014-12-03 19:30:05 +0100

    Clase RedSocial.

---
+++ AgendaDentista/src/RedSocial.cpp
@@ -0,0 +1,21 @@
+/*
+ * RedSocial.cpp
+ *
+ *  Created on: 03/12/2014
+ *      Author: i32ropie
+ */
+
+#include "RedSocial.h"
+
+namespace dentista {
+
+RedSocial::RedSocial() {
+	// TODO Auto-generated constructor stub
+
+}
+
+RedSocial::~RedSocial() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace dentista */
---
+++ AgendaDentista/src/RedSocial.h
@@ -0,0 +1,20 @@
+/*
+ * RedSocial.h
+ *
+ *  Created on: 03/12/2014
+ *      Author: i32ropie
+ */
+
+#ifndef REDSOCIAL_H_
+#define REDSOCIAL_H_
+
+namespace dentista {
+
+class RedSocial {
+public:
+	RedSocial();
+	virtual ~RedSocial();
+};
+
+} /* namespace dentista */
+#endif /* REDSOCIAL_H_ */
