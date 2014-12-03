5e134fbd18929a23e4a7f496b932ccd7b5a7099e
IGNACIO SANCHEZ JIMENEZ <nacho.26.5.1995@gmail.com>
2014-12-03 20:13:40 +0100

    Modificado RedSocial.h caxhondas

--- AgendaDentista/src/RedSocial.h
+++ AgendaDentista/src/RedSocial.h
@@ -11,9 +11,29 @@
 namespace dentista {
 
 class RedSocial {
+private:
+	std::string cuentaFacebook;
+	std::string cuentaTwitter;
+
 public:
 	RedSocial();
 	virtual ~RedSocial();
+
+	const std::string& getCuentaFacebook() const {
+		return cuentaFacebook;
+	}
+
+	void setCuentaFacebook(const std::string& cuentaFacebook) {
+		this->cuentaFacebook = cuentaFacebook;
+	}
+
+	const std::string& getCuentaTwitter() const {
+		return cuentaTwitter;
+	}
+
+	void setCuentaTwitter(const std::string& cuentaTwitter) {
+		this->cuentaTwitter = cuentaTwitter;
+	}
 };
 
 } /* namespace dentista */
