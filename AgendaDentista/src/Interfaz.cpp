813b720af351ed953ba93a8c57b93132abeaec1c
Eduardo Roldan Pijuan <i32ropie@uco.es>
2014-12-03 19:29:30 +0100

    Clase Interfaz.

---
+++ AgendaDentista/src/Interfaz.cpp
@@ -0,0 +1,21 @@
+/*
+ * Interfaz.cpp
+ *
+ *  Created on: 03/12/2014
+ *      Author: i32ropie
+ */
+
+#include "Interfaz.h"
+
+namespace dentista {
+
+Interfaz::Interfaz() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Interfaz::~Interfaz() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace dentista */
---
+++ AgendaDentista/src/Interfaz.h
@@ -0,0 +1,20 @@
+/*
+ * Interfaz.h
+ *
+ *  Created on: 03/12/2014
+ *      Author: i32ropie
+ */
+
+#ifndef INTERFAZ_H_
+#define INTERFAZ_H_
+
+namespace dentista {
+
+class Interfaz {
+public:
+	Interfaz();
+	virtual ~Interfaz();
+};
+
+} /* namespace dentista */
+#endif /* INTERFAZ_H_ */
