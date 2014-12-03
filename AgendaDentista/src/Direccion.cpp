2bd69ea061843b04e221ac1adc2b03dc96e823b3
Eduardo Roldan Pijuan <i32ropie@uco.es>
2014-12-03 19:29:04 +0100

    Clase Direccion.

---
+++ AgendaDentista/src/Direccion.cpp
@@ -0,0 +1,21 @@
+/*
+ * Direccion.cpp
+ *
+ *  Created on: 03/12/2014
+ *      Author: i32ropie
+ */
+
+#include "Direccion.h"
+
+namespace dentista {
+
+Direccion::Direccion() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Direccion::~Direccion() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace dentista */
---
+++ AgendaDentista/src/Direccion.h
@@ -0,0 +1,20 @@
+/*
+ * Direccion.h
+ *
+ *  Created on: 03/12/2014
+ *      Author: i32ropie
+ */
+
+#ifndef DIRECCION_H_
+#define DIRECCION_H_
+
+namespace dentista {
+
+class Direccion {
+public:
+	Direccion();
+	virtual ~Direccion();
+};
+
+} /* namespace dentista */
+#endif /* DIRECCION_H_ */
