f74895c6728977d9e8deef2d0e261bc7a02db6b4
Eduardo Roldan Pijuan <i32ropie@uco.es>
2014-12-03 19:29:50 +0100

    Clase Paciente.

---
+++ AgendaDentista/src/Paciente.cpp
@@ -0,0 +1,21 @@
+/*
+ * Paciente.cpp
+ *
+ *  Created on: 03/12/2014
+ *      Author: i32ropie
+ */
+
+#include "Paciente.h"
+
+namespace dentista {
+
+Paciente::Paciente() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Paciente::~Paciente() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace dentista */
---
+++ AgendaDentista/src/Paciente.h
@@ -0,0 +1,20 @@
+/*
+ * Paciente.h
+ *
+ *  Created on: 03/12/2014
+ *      Author: i32ropie
+ */
+
+#ifndef PACIENTE_H_
+#define PACIENTE_H_
+
+namespace dentista {
+
+class Paciente {
+public:
+	Paciente();
+	virtual ~Paciente();
+};
+
+} /* namespace dentista */
+#endif /* PACIENTE_H_ */
