77775ea71c8116560d89e74c5cee752f4604c9d3
Eduardo Roldan Pijuan <i32ropie@uco.es>
2014-12-26 18:21:48 +0100

    Añadida la creación de la agenda en el programa principal asociado a un fichero.

--- AgendaDentista/src/AgendaDentista.cpp
+++ AgendaDentista/src/AgendaDentista.cpp
@@ -10,6 +10,10 @@
 using namespace std;
 
 int main() {
+	// Fichero hereda de interfaz y por eso se admite como parámetro en el constructor
+	// de Agenda
+	Fichero* f = new Fichero("almacen.txt");
+	Agenda a = Agenda(f);
 	cout << "OranGITops" << endl; // prints OranGITops
 	return 0;
 }
