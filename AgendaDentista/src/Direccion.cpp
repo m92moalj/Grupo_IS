01f6ca9fe4a659f2059eaf24d4efd97ce3e97f61
Eduardo Roldan Pijuan <i32ropie@uco.es>
2014-12-27 17:59:25 +0100

    Actualizada clase Dirección.

--- AgendaDentista/src/Direccion.cpp
+++ AgendaDentista/src/Direccion.cpp
@@ -9,13 +9,51 @@
 
 namespace dentista {
 
-Direccion::Direccion() {
-	// TODO Auto-generated constructor stub
-
+const std::string& dentista::Direccion::getCalle() const {
+	return calle_;
 }
 
-Direccion::~Direccion() {
-	// TODO Auto-generated destructor stub
+void dentista::Direccion::setCalle(const std::string& calle) {
+	calle_ = calle;
 }
 
-} /* namespace dentista */
+const std::string& dentista::Direccion::getEscalera() const {
+	return escalera_;
+}
+
+void dentista::Direccion::setEscalera(const std::string& escalera) {
+	escalera_ = escalera;
+}
+
+int dentista::Direccion::getNumPiso() const {
+	return numPiso_;
+}
+
+void dentista::Direccion::setNumPiso(int numPiso) {
+	numPiso_ = numPiso;
+}
+
+const std::string& dentista::Direccion::getNumPortal() const {
+	return numPortal_;
+}
+
+void dentista::Direccion::setNumPortal(const std::string& numPortal) {
+	numPortal_ = numPortal;
+}
+
+const std::string& dentista::Direccion::getNumPuerta() const {
+	return numPuerta_;
+}
+
+void dentista::Direccion::setNumPuerta(const std::string& numPuerta) {
+	numPuerta_ = numPuerta;
+}
+
+const std::string& dentista::Direccion::getTipoVia() const {
+	return tipoVia;
+}
+
+void dentista::Direccion::setTipoVia(const std::string& tipoVia) {
+	this->tipoVia = tipoVia;
+}
+}
--- AgendaDentista/src/Direccion.h
+++ AgendaDentista/src/Direccion.h
@@ -5,16 +5,35 @@
  *      Author: i32ropie
  */
 
-#ifndef DIRECCION_H_
-#define DIRECCION_H_
+#ifndef DIRECCION_H
+#define DIRECCION_H
+#include <string>
 
 namespace dentista {
 
 class Direccion {
+private:
+	std::string calle_;
+	std::string numPortal_;
+	int numPiso_;
+	std::string numPuerta_;
+	std::string escalera_;
+	std::string tipoVia;
+
 public:
-	Direccion();
-	virtual ~Direccion();
+	const std::string& getCalle() const;
+	void setCalle(const std::string& calle);
+	const std::string& getEscalera() const;
+	void setEscalera(const std::string& escalera);
+	int getNumPiso() const;
+	void setNumPiso(int numPiso);
+	const std::string& getNumPortal() const;
+	void setNumPortal(const std::string& numPortal);
+	const std::string& getNumPuerta() const;
+	void setNumPuerta(const std::string& numPuerta);
+	const std::string& getTipoVia() const;
+	void setTipoVia(const std::string& tipoVia);
 };
 
-} /* namespace dentista */
-#endif /* DIRECCION_H_ */
+}
+#endif
